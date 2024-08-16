c67a29d9fa4bd45450e025008589c49a74c8e542
pkarellas <pkarella39@gmail.com>
2024-08-15 22:04:35 -0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,181 @@
+
+//array of questions
+const quiz = [
+{
+    q:'Is asphalt binder sampled from the last possible point before being introduced into the hotplant?',
+    options:['True','False'],
+    answer: 0
+},
+{
+    q:'Calculate the Dry Density?',
+    options:['129.1','121.9','130.1','125.6'],
+    answer: 1,
+    img: 'images/calc3new.png'
+},
+{
+    q:'A double friction top can is used to sample what type of material?',
+    options:['aggregates','emulsions','asphalt binder','soils'],
+    answer: 2,
+},
+{
+    q:'Sampling AC from finished pavement is taken through the complete thickness, or _______  of the pavement. In a way that causes minimum disturbance to the sample.',
+    options:['Jack Hammer','None of the above','Lift','Shovel full'],
+    answer: 2
+},
+{
+    q:'Calculate the Volume of the Hole?',
+    options:['NONE','.0600','.0614','.0641'],
+    answer: 2,
+    img: 'images/calc3newest.png'
+},
+{
+    q:'Sampling from the Roadway ____ shovelful of material is tken from at least _____ random locations.',
+    options:['1, 4','3, 1','1, 2','1, 3'],
+    answer: 3
+},
+{
+    q:'Can you use power equipment to develop seperate, small, sampling piles when sampling from stockpiles?',
+    options:['True','False'],
+    answer: 1
+},
+{
+    q:'Are these the correct spots to sample a stockpile from?',
+    options:['True','False'],
+    answer: 0,
+    img: 'images/big.png'
+},
+{
+    q:'When sampling a widrow remove _____ to ______ inches initially',
+    options:['3, 7','2, 6','3, 6','1, 6'],
+    answer: 2
+},
+{
+    q:'Are these the correct spots to sample a stockpile from?',
+    options:['True','False'],
+    answer: 1,
+    img: 'images/big1.png'
+},
+{
+    q:'Sample Containers should not be thoroughly cleaned with a solvent before use?',
+    options:['True','False'],
+    answer: 0
+},
+{
+    q:'Are these the correct spots to sample a stockpile from?',
+    options:['True','False'],
+    answer: 1,
+    img: 'images/big2.png'
+},
+{
+    q:'What kind of samples are obtained from locations and by means to provide approprite representation of the AC mixture being placed?',
+    options:['Miscellaneous Placement of AC','From the Stockpile','Acceptance Sample.','Asphalt Binder'],
+    answer: 0
+},
+{
+    q:'Are these the correct spots to sample a stockpile from?',
+    options:['True','False'],
+    answer: 1,
+    img: 'images/big3.png'
+},
+{
+    q:'When sampling asphaltic concrete where should the steel plate be placed?',
+    options:['no where','on top of the truck','approximately 1 foot from edge of mat','at the hot plant.'],
+    answer: 2
+},
+{
+    q:'Are these the correct spots to sample a stockpile from?',
+    options:['True','False'],
+    answer: 1,
+    img: 'images/big4.png'
+},
+{
+    q:'Record weight of baseplate, empty mold, and glass plate to at least the nearest?',
+    options:['0.2grams','0.1grams','1.0grams','0.5grams'],
+    answer: 1
+},
+
+{
+    q:'Calculate the Wt of Water to fill Mold?',
+    options:['941.9','941.1','950.0','940.0'],
+    answer: 1,
+    img: 'images/calc.png'
+},
+{
+    q:'Calculate the percent retained on the #4?',
+    options:['40%','45%','95%','None of the above.'],
+    answer: 0,
+    img: 'images/calc1.png'
+},
+{
+    q:'Calculate the Wet Density?',
+    options:['NONE','.0600','.0614','.0641'],
+    answer: 2,
+    img: 'images/calc2new.png'
+},
+{
+    q:'What temperature should the distlled water when calibrating a proctor mold?',
+    options:['77 +- 9 F','77 +- 9 C','73 +- 3 F','73 +-2 F'],
+    answer: 0
+},
+{
+    q:'The temperature of the water when calibrating a proctor mold should be to the nearest?',
+    options:['0.01 C','0.01 F','0.1 C','0.1 F'],
+    answer: 3
+},
+{
+    q:'When sampling from a stopped conveyor belt one should use a template shaped to the same contour of the belt, remove all material within the template area, and use a brush to obtain the fine particles?',
+    options:['True','False'],
+    answer: 0
+},
+{
+    q:'It is not necessary to know the temperature when calibrating for the volume of a proctor mold.',
+    options:['True','False'],
+    answer: 1
+},
+{
+    q:'When determing the weight of the sand to be used in the sand cone density procedure. Use a straight edge in ______?',
+    options:['0.01 C','0.01 F','0.1 C','0.1 F'],
+    answer: 3
+},
+{
+    q:'What size diameter mold is used when calibrating the weight per cubic foot of sand for the sand cone density test?',
+    options:['4 inch','6 inch','3 inch','5 inch'],
+    answer: 0
+},
+{
+    q:'What should emulsions be protected from?',
+    options:['Water','Lightning','Heat','Freezing'],
+    answer: 3
+},
+{
+    q:'It is perfectly fine to transfer samples of emulsion and binder from one container to another.',
+    options:['True','False'],
+    answer: 1
+    },
+{
+    q:'What procedure is being shown in the picture?',
+    options:['ARIZ 104','ARIZ 103','.ARIZ 229','ARIZ 105'],
+    answer: 2,
+    img: 'images/229.png'
+},
+{
+    q:'It is okay to have construction equipment operating in the immediate vicinity of the sand cone test.',
+    options:['True','False'],
+    answer: 1
+},
+{
+        q:'When referencing a Method A proctor while doing a Sand Cone test the material obtained from the hole is sieved over ____?',
+        options:['#10 and #12','3 inch and #4','3 inch and #8;','None of the Above'],
+        answer: 1
+},
+{
+        q:'It is okay to have construction equipment operating in the immediate vicinity of the sand cone test.',
+        options:['True','False'],
+        answer: 1
+},
+{
+        q:'Which piece of equipment below is utilized to standardize a nuclear gauge?',
+        options:['<img src="images/newblocks.png">','<img src="images/plate.png">','<img src="images/box.png">'],
+        answer: 1
+},
+]
