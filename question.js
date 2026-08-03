08fc37788f2c76fdffd5767fca80810e7651a79a
pkarellas <pkarella39@gmail.com>
2026-08-03 13:23:04 -0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,149 @@
+
+//array of questions
+const quiz = [
+{
+    q:'What is the procedure for SAMPLING AGGREGATE PRODUCTS in AASHTO?',
+    options:['AASHTO R97','AASHTO R90', 'AASHTO T89', 'AASHTO T90'],
+    answer: 1
+},
+{
+    q:'Make sure all equipment and containers are _____ and ______ before sampling during AASHTO R90?',
+    options:['None of the above','clean AND wet','dusty AND wet','clean AND dry'],
+    answer: 3,
+   
+},
+{
+    q:'In AASHTO R90 Sampling Aggregate products, where is the minimum recommended sample sizes located for field samples?',
+    options:['TABLE 3 in the procedure','up to the field tech','TABLE 2 in the procedure','TABLE 1 in the procedure'],
+    answer: 3,
+},
+{
+    q:'Sample size is based upon the test(s) required for that material?',
+    options:['True','False'],
+    answer: 0
+},
+{
+    q:'If the nominal maximum aggregate size is 3/4" what is the minimum mass in lbs (AASHTO R90)',
+    options:['12lbs','20lbs','30lbs','8lbs'],
+    answer: 2,
+    img: 'images/table1r90new.png'
+},
+{
+    q:'What is the first step when SAMPLING FROM CONVEYOR BELT USING A TEMPLATE according to AASHTO R90?',
+    options:['set sampling template on belt','clean material from inside template','none of these answers','stop the belt'],
+    answer: 3
+},
+{
+    q:'When sampling aggregate from the conveyor belt you can sample from the beginning or the end of the aggregate run according to AASHTO R90. ',
+    options:['True','False'],
+    answer: 1
+},
+{
+     q:'Which piece of equipment below is NOT used according to AASHTO R90?',
+    options:['<img src="images/paver.png">','<img src="images/shoeler.png">','<img src="images/photoadjustmentsforwebsite.png">'],
+     answer: 0
+},
+
+{
+    q:'When sampling from the back of a transport unit (truck) what illustration is accurate according to AASHTO R90?',
+    options:['<img src="images/truck2spot.png">','<img src="images/truck4spot.png">','<img src="images/truck2spot.png">'],
+    answer: 1
+},
+
+{
+    q:'When sampling from a windrow remove the top _____  before taking your increment sample according to AASHTO R90?',
+    options:['1/3','1/2','1/4','1/16'],
+    answer: 0
+},
+
+{
+    q:'When sampling from a windrow obtain a minimum of _____ approximately equal increments from _____ locations according to AASHTO R90?',
+    options:['3, known','3, random','2, known','2, random'],
+    answer: 1
+},
+{
+    q:'When sampling from a transport, remove approximately _____ of material from the top of the sampling area before obtaining the first increment of your sample. according to AASHTO R90?',
+    options:['6 inches','1 foot','1 inch','3 feet'],
+    answer: 1
+},
+{
+    q:'When sampling from a conveyor belt with a template, set the sampling template on the belt in a way that _______ adjacent material from falling into the space inside the template according to AASHTO R90?',
+    options:['ensures','avoids','none of these answers','includes'],
+    answer: 1
+},
+{
+    q:'The image below demonstrates a method in AASHTO R90 (sampling aggregates) chose the correct method listed?',
+    options:['none of these answers','sampling from conveyor belt discharge','sampling from a stockpile','sampling from conveyor belt using a template'],
+    answer: 3,
+    img: 'images/templatemethod.png'
+},
+{
+    q:'When sampling from a conveyor belt with a template, remove the material from INSIDE the template; DO NOT include any material adhering to the belt according to AASHTO R90?',
+    options:['true','false'],
+    answer: 0
+},
+{
+    q:'When sampling from a conveyor belt with a template, remove the material from INSIDE the template; including any material adhering to the belt according to AASHTO R90?',
+    options:['true','false'],
+    answer: 1
+},
+{
+    q:'Field samples should meet or exceed TABLE 1 found in the procedure according to AASHTO R90?',
+    options:['true','false'],
+    answer: 0
+},
+{
+    q:'A portion of material that is collected by a sampling device in a single operation is called according to AASHTO?',
+    options:['stockpile','increment','none of these answers','windrow'],
+    answer: 1
+},
+
+{
+    q:'When sampling from a conveyor belt discharge using a sampling device (manual or automatic), the sampling device must be passed through the stream of material _______ as it runs off conveyor belt according to AASHTO R90?',
+    options:['halfway','fully','none of these answers','quarterway'],
+    answer: 1
+},
+{
+    q:'If a single increment is not enough for a sample _______ (AASHTO R90)?.',
+    options:['stop the process and start again.','repeat the process obtaining approximately equal increments each time.','none of these answers','chose a different method.'],
+    answer: 1
+},
+{
+    q:'Can several increments be combined to form a single sample according to AASHTO R90?.',
+    options:['True','False'],
+    answer: 0
+},
+{
+    q:'When sampling with a device from a conveyor belt discharge, pass through the full stream of material ______ in each direction at a _______ speed and ________ to the flow of the material without overflowing. (AASHTO R90)',
+    options:['once, constant, parallel','once, constant, perpendicular','once, rapid, perpendicular','twice, rapid, perpendicular'],
+    answer: 2
+},
+{
+    q:'When sampling aggregate from the conveyor belt avoid sampling from the beginning or the end of the aggregate run to avoid segregation of material.',
+    options:['True','False'],
+    answer: 0
+},
+{
+    q:'Visually divide the transport unit into ______ quadrants.(AASHTO R90: SAMPLING FROM TRANSPORT UNITS)',
+    options:['3','2','4','2'],
+    answer: 2
+},
+{
+    q:'Once approximately 1 ft of material is removed from the top of the sampling area. Obtain an increment from the exposed surface and repeat the process in each of the remaining quadrants. Combine increments into one sample.(AASHTO R90: SAMPLING FROM TRANSPORT UNITS)',
+    options:['3','2','4','2'],
+    answer: 2
+},
+{
+    q:'The ILLUSTRATION below is an aggregate windrow, the red dots are sampling locations. Is this image accurate according to AASHTO R90?',
+    options:['True','False'],
+    answer: 0,
+    img: 'images/fieldwindrowsL.png'
+},
+{
+    q:'The ILLUSTRATION below is an aggregate windrow, the red dots are sampling locations. Is this image accurate according to AASHTO R90?',
+    options:['True','False'],
+    answer: 1,
+    img: 'images/fieldwindrowsR.png'
+},
+
+]
